7823fe0708a34d4a6dfef29f8680c95d5848d89b
Gligard <lgcf97@gmail.com>
2024-03-20 17:50:25 -0300

    Initial commit

---
+++ src/main/java/com/alura/coursecase/CourseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.alura.coursecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/coursecase/CourseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.coursecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
